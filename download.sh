4de1a7509b5f49ebb175462880db4dc7f67a9c14
Alexander Sologub <alexsologoub@gmail.com>
2018-08-13 13:10:30 +0300

    Add Pandas and PyLint

--- download.sh
+++ download.sh
@@ -9,6 +9,10 @@ cd ./data
 kaggle competitions download -w -c airbus-ship-detection
 
 unzip test.zip -d ./test
-unzip train.zip -d ./train
 rm test.zip
+unzip train.zip -d ./train
 rm train.zip
+unzip train_ship_segmentations.csv.zip
+rm train_ship_segmentations.csv.zip
+unzip sample_submission.csv.zip
+rm sample_submission.csv.zip
