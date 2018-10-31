0d50f40196e8d2e686cff9d7baa9fba9771067d4
Alex Sologub <alexsologoub@gmail.com>
2018-10-31 21:33:32 +0300

    Crop before caching

--- airbus/pipelines.py
+++ airbus/pipelines.py
@@ -10,17 +10,15 @@ def channels_first(image):
 def resize(size, interpolation, image):
     return cv2.resize(image, size, interpolation=interpolation)
 
-def read_image_and_mask(mask_db, target_shape, path):
+def read_image_and_mask(mask_db, path):
     image = read_image(path)
     mask = load_mask(mask_db, image.shape[:2], path)
-    image = resize(target_shape, cv2.INTER_LINEAR, image)
-    if mask is not None:
-        mask = resize(target_shape, cv2.INTER_NEAREST, mask)
-    return image, mask
+    args = Crop(0, 0, 256, 256)(image=image, mask=mask)
+    return args['image'], args.get('mask')
 
-def read_image_and_mask_cached(cache, mask_db, target_shape, path):
+def read_image_and_mask_cached(cache, mask_db, path):
     if cache.get(path): return cache[path]
-    image, mask = read_image_and_mask(mask_db, target_shape, path)
+    image, mask = read_image_and_mask(mask_db, path)
     cache[path] = (image, mask)
     return image, mask
 
@@ -30,18 +28,16 @@ class ChannelsFirst:
         return args
 
 def train_pipeline(cache, mask_db, path):
-    image, mask = read_image_and_mask_cached(cache, mask_db, (768, 768), path)
+    image, mask = read_image_and_mask_cached(cache, mask_db, path)
     args = Compose([
-        Crop(0, 0, 256, 256),
         Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ChannelsFirst()
     ])(image=image, mask=mask)
     return args['image'], args.get('mask')
 
 def validation_pipeline(cache, mask_db, path):
-    image, mask = read_image_and_mask_cached(cache, mask_db, (768, 768), path)
+    image, mask = read_image_and_mask_cached(cache, mask_db, path)
     args = Compose([
-        Crop(0, 0, 256, 256),
         Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ChannelsFirst()
     ])(image=image, mask=mask)
