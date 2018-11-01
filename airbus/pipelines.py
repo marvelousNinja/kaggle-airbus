eaf2ae8d0cbb6c12547ccbadde9636456363f6e2
Alex Sologub <alexsologoub@gmail.com>
2018-11-01 23:45:10 +0300

    Add some augs

--- airbus/pipelines.py
+++ airbus/pipelines.py
@@ -1,6 +1,11 @@
 import cv2
 import numpy as np
-from albumentations import Compose, Crop, Normalize
+
+from albumentations import (Blur, Compose, Crop, GaussNoise,
+                            HorizontalFlip, MedianBlur,
+                            MotionBlur, Normalize, OneOf,
+                            RandomBrightness, RandomGamma, RandomSizedCrop,
+                            RandomRotate90, ShiftScaleRotate)
 
 from airbus.utils import load_mask, read_image
 
@@ -30,6 +35,22 @@ def train_pipeline(mask_db, path):
     image, mask = read_image_and_mask(mask_db, path)
     args = Compose([
         Crop(0, 0, 256, 256),
+        HorizontalFlip(p=0.5),
+        RandomRotate90(p=0.5),
+        OneOf([
+            ShiftScaleRotate(),
+            RandomSizedCrop(min_max_height=(200, 240), height=256, width=256)
+        ], p=0.2),
+        GaussNoise(p=0.2),
+        OneOf([
+            RandomBrightness(limit=0.2),
+            RandomGamma(),
+        ], p=0.5),
+        OneOf([
+            Blur(),
+            MedianBlur(),
+            MotionBlur()
+        ], p=0.2),
         Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ChannelsFirst()
     ])(image=image, mask=mask)
